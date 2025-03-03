bdac1164a6d7847dde46eb0c86c724a290eff420
AyhamAlkhatib <ayhamalkhatib25@gmail.com>
2025-03-03 16:20:41 -0500

    Update modify.php

--- Backend/modify.php
+++ Backend/modify.php
@@ -13,18 +13,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $dept_name = $_POST["dept_name"];
 
-    // TODO: Execute the query to get the Department name
-    $check_dept_name = "SELECT dept_name FROM department WHERE dept_name = '$dept_name'";
-    $result = $conn->query($check_dept_name);
-
-    // TODO: Checkk if the dept exists, and assign it to NULL if it's not
-    if ($result->num_rows == 0) {
-        $dept_name = NULL;  
+    if ($dept_name != "Miner School of Computer & Information Sciences") {
+        header("Location: main.html");
+        echo "Invalid Department name";
     }
 
     // TODO: Update the student's data
     $sql = "UPDATE student 
-            SET name = '$name', email = '$email', dept_name = " . ($dept_name === NULL ? 'NULL' : "'$dept_name'") . " 
+            SET name = '$name', email = '$email', dept_name = '$dept_name' 
             WHERE student_id = '$student_id'";
     
      $conn->query($sql);
